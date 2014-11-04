c9d43bbc4607b4abe764b1e0bb75ea634f3786a7
janfietz <janfietz@gmail.com>
2014-11-04 20:05:37 +0000

    tmb_module_led: initialize neopixel

--- toddlermusicbox/tmb_module_led.py
+++ toddlermusicbox/tmb_module_led.py
@@ -7,22 +7,24 @@ import time, threading
 import tmb_module, tmb_main
 import logging
 import collections
-
+import Queue
+from neopixel import *
         
 class LedThread(threading.Thread):
     '''
     classdocs
     '''
 
-    def __init__(self, mpc, host, port):
+    def __init__(self, strip):
         '''
         Constructor
         '''
         threading.Thread.__init__(self)
         
         self._loop = True
+        self._strip = strip
 
-        self.tasks = collections.deque()
+        self.tasks = Queue.Queue()
 
     @property
     def loop(self):
@@ -33,7 +35,21 @@ class LedThread(threading.Thread):
         self._loop = value
 
     def addTask(self, task):
-        self.tasks.append(task)
+        self.tasks.put_no_wait(task)
+
+    def _process(self, task):
+        exec('self._strip.' + task.popleft())
+
+    def run(self):
+    
+        self.loop = True
+        while self.loop:
+            try:
+                task = self.tasks.get(block = True, timeout = 1)
+                self._process(task)
+            except Queue.Empty:
+                pass 
+                
 
 class LedModule(tmb_module.TMB_Module):
     
@@ -43,24 +59,42 @@ class LedModule(tmb_module.TMB_Module):
         '''
         tmb_module.TMB_Module.__init__(self, config)
         
-        self.count = config.getint('led', 'count')
-        self.pin = config.getint('led', 'pin')
-        self.frequenz = config.getint('led', 'frequenz')
-        self.dma = config.getint('led', 'dma')
-        self.invert = config.getbool('led', 'invert')
-        
+        self._count = config.getint('led', 'count')
+        self._pin = config.getint('led', 'pin')
+        self._frequenz = config.getint('led', 'frequenz')
+        self._dma = config.getint('led', 'dma')
+        self._invert = config.getboolean('led', 'invert')
+        self._strip = None
                 
     def start(self):
         tmb_module.TMB_Module.start(self)
-        self.thread = MPCThread(self.mpc, self.host, self.port)
+        self._strip = Adafruit_NeoPixel(self._count, self._pin, self._frequenz, self._dma, self._invert)
+        self._strip.begin()
+
+        ''' initialize led '''
+        self._strip.setBrightness(200)
+        for i in range(self._count):
+            self._strip.setPixelColor(i, Color(0,0,255))
+        self._strip.show()
+
+        self._thread = LedThread(self._strip)
         
-        self.thread.start()
+        self._thread.start()
         
     def stop(self):
         print("Stop LedModule")
-        self.thread.loop = False
-        self.thread.join()
+        self._thread.loop = False
+        self._thread.join()
         tmb_module.TMB_Module.stop(self)
+
+    def setBrightness(self, brightness):
+        self.thread.setBrightness('setBrightness({})'.format(brightness))
+
+    def setLedColor(self, n, color):
+        self.thread.setBrightness('setPixelColor({}, {})'.format(n, color))
+
+    def show(self):
+        self.thread.setBrightness('show()')
 
         
          
