2bcb4cf7fecebfb6153aa74330cb32662f2ed9e2
janfietz <janfietz@gmail.com>
2014-12-31 08:01:48 +0100

    setup: set version to 1.0.0
    -add missing required modules

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup
 
 setup(name = 'toddlerMusicBox',
-	version = '0.0.1',
+	version = '1.0.0',
 	author = 'Jan Fietz',
 	author_email = 'janfietz@gmail.com',
 	description = 'Raspberry Pi based music box.',
 	packages=['toddlermusicbox'],
 	url = 'https://github.com/janfietz/toddlerMusicBox.git',
-	requires = ['neopixel', 'numpy', 'mpd'],
+	requires = ['neopixel', 'numpy', 'mpd', 'alsaaudio', 'nxppy', 'json', 'RPi.GPIO'],
 	scripts=['bin/toddlermusicbox']
 )
